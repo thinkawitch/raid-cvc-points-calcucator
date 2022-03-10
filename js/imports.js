8638878cb50fabd201c70e3d850cb5675c30854b
Andrew <andrew12.hk@gmail.com>
2022-03-10 17:55:25 +0300

    devtool added

--- js/imports.js
+++ js/imports.js
@@ -1,4 +1,9 @@
 // latest
+const shouldLoadDT = new URLSearchParams(window.location.search).has('dev');
+console.log('should load devtool?', shouldLoadDT);
+if (shouldLoadDT) {
+    await import('https://unpkg.com/preact@latest/devtools/dist/devtools.module.js?module');
+}
 import { h, render, createContext } from 'https://unpkg.com/preact@latest?module';
 import { useContext, useState, useReducer } from 'https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module';
 import htm from 'https://unpkg.com/htm@latest/dist/htm.module.js?module';
